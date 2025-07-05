50cf244ba9ac7aa1e430c2a2af36af252c6753b0
Fiducia Inc <mail@fiducia.gy>
2025-07-05 02:16:47 -0400

    Add fillable property with relationship

--- app/Models/ReportUploads.php
+++ app/Models/ReportUploads.php
@@ -9,4 +9,16 @@ class ReportUploads extends Model
 {
     /** @use HasFactory<\Database\Factories\ReportUploadsFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'report_id',
+        'file_url',
+        'file_type',
+        'uploaded_by',
+    ];
+
+    public function report()
+    {
+        return $this->belongsTo(Report::class);
+    }
 }
