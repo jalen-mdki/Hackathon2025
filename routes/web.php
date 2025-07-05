be7415f29a66ea726b5a84b2ed125150506c1b33
Fiducia Inc <mail@fiducia.gy>
2025-07-05 03:08:23 -0400

    Add reports routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\Admin\OrganizationController;
+use App\Http\Controllers\Admin\ReportController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -20,6 +21,16 @@ Route::middleware(['auth', 'verified', 'can:admin-access'])->prefix('admin')->na
     Route::get('organizations/{organization}/edit', [OrganizationController::class, 'edit'])->name('organizations.edit');
     Route::put('organizations/{organization}', [OrganizationController::class, 'update'])->name('organizations.update');
     Route::get('organizations/{organization}', [OrganizationController::class, 'show'])->name('organizations.show');
+
+    Route::prefix('reports')->name('reports.')->group(function () {
+        Route::get('/', [ReportController::class, 'index'])->name('index');
+        Route::get('/create', [ReportController::class, 'create'])->name('create');
+        Route::post('/', [ReportController::class, 'store'])->name('store');
+        Route::get('/{report}', [ReportController::class, 'show'])->name('show');
+        Route::get('/{report}/edit', [ReportController::class, 'edit'])->name('edit');
+        Route::put('/{report}', [ReportController::class, 'update'])->name('update');
+        Route::delete('/{report}', [ReportController::class, 'destroy'])->name('destroy');
+    });
 });
 
 // Non-admin (general) organization routes placeholder for future expansion
