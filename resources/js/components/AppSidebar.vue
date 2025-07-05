621d872addce6e633911c6f420773960192c2e12
Fiducia Inc <mail@fiducia.gy>
2025-07-05 03:09:08 -0400

    Add Report To SideBar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Building, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Building, Clipboard, ClipboardList, Folder, LayoutGrid } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -18,6 +18,11 @@ const mainNavItems: NavItem[] = [
         title: 'Organizations',
         href: '/admin/organizations',
         icon: Building
+    },
+    {
+        title: 'Reports',
+        href: '/admin/reports',
+        icon: ClipboardList
     }
 ];
 
