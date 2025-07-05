4eaa6f3a415b2dd841f0fa5d96e32520e0e6076d
Fiducia Inc <mail@fiducia.gy>
2025-07-05 02:16:18 -0400

    Add relationships

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -9,4 +9,30 @@ class Organization extends Model
 {
     /** @use HasFactory<\Database\Factories\OrganizationFactory> */
     use HasFactory;
+
+    public function reports()
+    {
+        return $this->hasMany(Report::class);
+    }
+
+    public function employees()
+    {
+        return $this->belongsToMany(User::class, 'organization_users');
+    }
+
+    public function hazards()
+    {
+        return $this->hasMany(Hazards::class);
+    }
+
+    public function emergencyResponsePlans()
+    {
+        return $this->hasMany(EmergencyResponsePlans::class);
+    }
+
+    public function userTrainings()
+    {
+        return $this->hasMany(UserTraining::class);
+    }
+
 }
